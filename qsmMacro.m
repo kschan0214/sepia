c23de44804715af0aaf92fe98693a115f8fa8515
Kwok-shing Chan <k.chan@donders.ru.nl>
2017-06-28 21:27:06 +0200

    add STI suite iLSQR

--- qsmMacro.m
+++ qsmMacro.m
@@ -3,15 +3,22 @@
 % Description: Wrapper for QSM (default using TKD)
 %   Flags:
 %       'method'        : QSM method, 
-%                          'TKD', 'ClosedFormL2' and 'iLSQR'
-%       'threshold'     : threshold for TKD (TKD)
+%                          'TKD', 'ClosedFormL2', 'iLSQR' and
+%                          'STISuiteiLSQR'
+%       'threshold'     : threshold for TKD (TKD or STISuiteiLSQR)
 %       'lambda'        : regularisation value (ClosedFormL2 or iLSQR)
 %       'optimise'      : self-define regularisation based on curvature of 
 %                         L-curve (ClosedFormL2)
 %       'tol'           : error tolerance (iLSQR)
-%       'iteration' 	: no. of maximum iteration for iLSQR (iLSQR)
+%       'iteration' 	: no. of maximum iteration for iLSQR (iLSQR or STISuiteiLSQR)
 %       'weight'    	: weighting of error computation (iLSQR)
 %       'initGuess'     : initial guess for iLSQR (iLSQR)
+%       'tol_step1'     : error tolerance (STISuiteiLSQR)
+%       'tol_step2'     : error tolerance (STISuiteiLSQR)
+%       'b0dir'         : main magnetic field direction (STISuiteiLSQR)
+%       'TE'            : echo time (STISuiteiLSQR)
+%       'fieldStrength' : magntic field strength of the scanner (STISuiteiLSQR)
+%       'padsize'       : size for padarray to increase numerical accuracy (STISuiteiLSQR)
 %
 % Kwok-shing Chan @ DCCN
 % k.chan@donders.ru.nl
@@ -34,6 +41,10 @@ if ~isempty(varargin)
                 case 'ilsqr'
                     [method, lambda, tol, maxiter, wmap, initGuess] = parse_vararginiLSQR(varargin);
                     break
+                case 'stisuiteilsqr'
+                    [method, algoPara] = parse_vararginSTISuiteiLSQR(varargin);
+                    algoPara.voxelsize= voxelSize;
+                    break
             end
         end
     end
@@ -55,6 +66,8 @@ switch method
         chi = qsmIterativeLSQR(localField,mask,matrixSize,voxelSize,...
             'lambda',lambda,'tol',tol,'iteration',maxiter,'weight',wmap,...
             'initGuess',initGuess);
+    case 'STISuiteiLSQR'
+        chi = QSM_iLSQR(localField,mask,'params',algoPara);
 end
 
 end
@@ -117,6 +130,54 @@ for kvar = 1:length(arg)
     end
     if strcmpi(arg{kvar},'initGuess')
         initGuess = arg{kvar+1};
+        continue
+    end
+end
+end
+
+% STI suite iLSQR
+function [method, params] = parse_vararginSTISuiteiLSQR(arg)
+method = 'STISuiteiLSQR';
+params.H=[0 0 1];
+params.niter=100;
+params.TE=1;
+params.B0=3;
+params.tol_step1=0.01;
+params.tol_step2=0.001;
+params.Kthreshold=0.25;
+params.padsize=4;
+
+for kvar = 1:length(arg)
+    if strcmpi(arg{kvar},'b0dir')
+        params.H = arg{kvar+1};
+        continue
+    end
+    if strcmpi(arg{kvar},'tol_step1')
+        params.tol_step1 = arg{kvar+1};
+        continue
+    end
+    if strcmpi(arg{kvar},'tol_step2')
+        params.tol_step2 = arg{kvar+1};
+        continue
+    end
+    if strcmpi(arg{kvar},'iteration')
+        params.niter = arg{kvar+1};
+        continue
+    end
+    if strcmpi(arg{kvar},'TE')
+        params.TE = arg{kvar+1};
+        continue
+    end
+    if strcmpi(arg{kvar},'fieldStrength')
+        params.B0 = arg{kvar+1};
+        continue
+    end
+    if strcmpi(arg{kvar},'threshold')
+        params.Kthreshold = arg{kvar+1};
+        continue
+    end
+    if strcmpi(arg{kvar},'padsize')
+        params.padsize = arg{kvar+1};
         continue
     end
 end
