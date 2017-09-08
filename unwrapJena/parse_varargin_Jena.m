97c6871dbefb24d51e83dde1ae61f1e4e3e51a90
Kwok-shing Chan <k.chan@donders.ru.nl>
2017-09-08 12:42:06 +0200

    add parsers

---
+++ unwrapJena/parse_varargin_Jena.m
@@ -0,0 +1,18 @@
+%% function [mask] = parse_varargin_Jena(arg)
+%
+% Description: parser for UnwrapPhaseMacro.m
+%
+% Kwok-shing Chan @ DCCN
+% k.chan@donders.ru.nl
+% Date created: 8 September 2017
+% Date last modified: 
+%
+function [mask] = parse_varargin_Jena(arg)
+mask = [];
+for kkvar = 1:length(arg)
+    if strcmpi(arg{kkvar},'mask')
+        mask = arg{kkvar+1};
+        continue
+    end
+end
+end
