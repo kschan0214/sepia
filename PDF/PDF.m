b92ac1a88a2e07e62be060cb310ad57930259117
Kwok-shing Chan <k.chan@donders.ru.nl>
2017-09-08 14:24:45 +0200

    bug fix

--- PDF/PDF.m
+++ PDF/PDF.m
@@ -28,31 +28,12 @@
 %   Last modified by Tian Liu on 2013.07.24
 %
 %   Modified: Kwok-shing Chan @ dccn
-%   Date modified: 28 June 2017
+%   Date modified: 8 September 2017
 
 
-function [RDF, shim] = PDF(iFreq, N_std, Mask,matrix_size,voxel_size, B0_dir,varargin)
+function [RDF, shim] = PDF(iFreq,mask,matrix_size,voxel_size,varargin)
 % predefined parameters
-tol = 0.1;
-itermax = 30;
-isMEDICG = true;
-% obtain flags values
-if ~isempty(varargin)
-    for kvar=1:length(varargin)
-        if strcmpi(varargin{kvar},'tol')
-            tol = varargin{kvar+1};
-            continue
-        end
-        if strcmpi(varargin{kvar},'iteration')
-            itermax = varargin{kvar+1};
-            continue
-        end
-        if strcmpi(varargin{kvar},'CGsolver')
-            isMEDICG = varargin{kvar+1};
-            continue
-        end
-    end
-end
+[B0_dir, tol, itermax, isMEDICG, N_std] = parse_varargin_PDF(varargin);
 
 % if (nargin<7)
 %     tol = 0.1;
@@ -60,15 +41,15 @@ end
 
 % zero pad
 matrix_size0 = matrix_size;
-d1 = max(max(Mask,[],2),[],3);
+d1 = max(max(mask,[],2),[],3);
 d1first = find(d1,1,'first');
 d1last = find(d1,1,'last');
 
-d2 = max(max(Mask,[],1),[],3);
+d2 = max(max(mask,[],1),[],3);
 d2first = find(d2,1,'first');
 d2last = find(d2,1,'last');
         
-d3 = max(max(Mask,[],1),[],2);
+d3 = max(max(mask,[],1),[],2);
 d3first = find(d3,1,'first');
 d3last = find(d3,1,'last');
 
@@ -77,32 +58,33 @@ matrix_size = [ floor((d1last - d1first+40)/2)*2,...
                 floor((d3last - d3first+40)/2)*2];
 iFreq=iFreq(d1first:d1last,d2first:d2last, d3first:d3last);
 N_std=N_std(d1first:d1last,d2first:d2last, d3first:d3last);
-Mask=Mask(d1first:d1last,d2first:d2last, d3first:d3last);
+mask=mask(d1first:d1last,d2first:d2last, d3first:d3last);
 padsize = [matrix_size(1)-size(iFreq,1) matrix_size(2)-size(iFreq,2) matrix_size(3)-size(iFreq,3)];
 iFreq = padarray(iFreq, padsize, 0,'post');
 N_std = padarray(N_std, padsize, 0,'post');
-Mask = padarray(Mask, padsize, 0,'post');
+mask = padarray(mask, padsize, 0,'post');
 
 
 % generate the weighting
 W = 1./N_std;
 W(isinf(W)) =0;
-W = W.*(Mask>0);
+W = W.*(mask>0);
 W_std = W;
 W_var = W.^2;
 
 %%%%%% start the PDF method %%%%%
-D = dipole_kernel(matrix_size,voxel_size,B0_dir,'imagespace');
+% D = dipole_kernel(matrix_size,voxel_size,B0_dir,'imagespace');
+D = dipole_kernel(matrix_size,voxel_size,B0_dir,'kspace');
 
 % generating the RHS vector in Eq. 6 in the PDF paper
 p_temp = real(ifftn(D.*fftn(W_var.*(iFreq) )));
-b = p_temp( Mask(:) == 0);
+b = p_temp( mask(:) == 0);
 
 % set erance level and maximum iteration allowed
 E_noise_level = real(ifftn(D.*fftn(W_std.*ones(size(N_std)))));
 % itermax= 30;
-A=@(xx)(dipole_term(W_var,D,Mask,xx) );
-cg_tol = tol*norm(E_noise_level( Mask(:) == 0))/norm(b(:));
+A=@(xx)(dipole_term(W_var,D,mask,xx) );
+cg_tol = tol*norm(E_noise_level( mask(:) == 0))/norm(b(:));
 
 if isMEDICG
     [x res num_iter] = cgsolve(A, b, cg_tol, itermax, 0);
@@ -111,14 +93,14 @@ else
 end
  
 xx = zeros(size(D));
-xx(Mask(:) == 0) = x(1:end);
-xx(Mask(:) > 0) = 0;
+xx(mask(:) == 0) = x(1:end);
+xx(mask(:) > 0) = 0;
 
 % background dipole field
 p_dipole = real(ifftn(D.*fftn(xx)));
 
 
-p_final = (iFreq-p_dipole).*Mask;  
+p_final = (iFreq-p_dipole).*mask;  
 
 % remove zero pad
 RDF = zeros(matrix_size0);
