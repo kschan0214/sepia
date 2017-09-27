61349147dc37782b6ffe9972e36dd0648c2c0920
Kwok-Shing Chan <kwocha@mentat003.dccn.nl>
2017-09-27 14:39:14 +0200

    change ndgrid to meshgrid

--- qsm_algorithm/utils/DipoleKernel.m
+++ qsm_algorithm/utils/DipoleKernel.m
@@ -1,4 +1,4 @@
-%% function dipoleKernel = DipoleKernel(matrixSize,voxelSize,b0dir)
+%% function [dipoleKernel,dKComponents] = DipoleKernel(matrixSize,voxelSize,b0dir)
 %
 % Description: Create dipole kernel in k-space with input matrix dimensions
 %              and spatial resolution
@@ -10,23 +10,27 @@
 %
 % Output
 % ______
-%   dipoleKernal                : Dipole kernel (in k-space)
+%   dipoleKernal      : Dipole kernel (in k-space)
+%   dKComponents      : dipole kernel components
 %
 % Kwok-shing Chan @ DCCN
 % k.chan@donders.ru.nl
 % Date created: 24 March 2017
-% Date last modified: 26 September 2017
+% Date last modified: 27 September 2017
 %
-function dipoleKernel = DipoleKernel(matrixSize,voxelSize,b0dir)
+function [dipoleKernel,dKComponents] = DipoleKernel(matrixSize,voxelSize,b0dir)
 
 if isempty(b0dir)
     b0dir = [0 0 1];
 end
 
 % KC: create 3D matrix in k-space
-[ky,kx,kz] = ndgrid(-matrixSize(1)/2:matrixSize(1)/2-1, ...
-                    -matrixSize(2)/2:matrixSize(2)/2-1, ...
-                    -matrixSize(3)/2:matrixSize(3)/2-1);
+% [ky,kx,kz] = ndgrid(-matrixSize(1)/2:matrixSize(1)/2-1, ...
+%                     -matrixSize(2)/2:matrixSize(2)/2-1, ...
+%                     -matrixSize(3)/2:matrixSize(3)/2-1);
+[ky,kx,kz] = meshgrid(-matrixSize(2)/2:matrixSize(2)/2-1, ...
+                      -matrixSize(1)/2:matrixSize(1)/2-1, ...
+                      -matrixSize(3)/2:matrixSize(3)/2-1);
 
 % KC: assign k-vectors
 kx = (kx / max(abs(kx(:)))) / voxelSize(1);
@@ -40,6 +44,12 @@ k2 = kx.^2 + ky.^2 + kz.^2;
 %     k-vector and the static B field
 % dipoleKernel = fftshift( 1/3 - (kz ).^2 ./ (k2 + eps) );
 % 260917: correct for B0 direction, b0dir = [x,y,z]; 
-dipoleKernel = fftshift( 1/3 - (kx*b0dir(2) + ky*b0dir(1) + kz*b0dir(3)).^2 ./ (k2 + eps) );
+% dipoleKernel = fftshift( 1/3 - (kx*b0dir(2) + ky*b0dir(1) + kz*b0dir(3)).^2 ./ (k2 + eps) );
+dipoleKernel = fftshift( 1/3 - (kx*b0dir(1) + ky*b0dir(2) + kz*b0dir(3)).^2 ./ (k2 + eps) );
+
+dKComponents.kx = kx;
+dKComponents.ky = ky;
+dKComponents.kz = kz;
+dKComponents.k2 = k2;
 
 end
