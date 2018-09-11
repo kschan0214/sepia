3bffb8cc6f765d7865ae22222748f926670e88d8
Kwok-shing Chan <k.chan@donders.ru.nl>
2018-09-11 16:08:44 +0200

    update nifti io

--- utils/nifti/utils/save_nii_img_only.m
+++ utils/nifti/utils/save_nii_img_only.m
@@ -7,9 +7,28 @@
 % Date created: 24 January 2017
 % Date last modified:
 %
-function save_nii_img_only(headerfilename,savefilename,images)
-load_module_NIfTI;
+function save_nii_img_only(headerfilename,savefilename,img,datatype)
+
+if nargin < 4
+    datatype = 16;
+end
+
 nii = load_untouch_nii(headerfilename);
-nii.img = images;
+nii.img = single(img);
+nii.hdr.dime.datatype = datatype;
+nii.hdr.dime.dim(5) = size(img,4);
+if size(img,4) > 1
+    nii.hdr.dime.dim(1) = 4;
+end
+
+% assume the input image contains the true values
+nii.hdr.dime.scl_inter = 0;
+nii.hdr.dime.scl_slope = 1;
+
+% if nii.hdr.dime.datatype ==4
+%     nii.hdr.dime.datatype = 16;
+% end
+% nii.img = images;
+
 save_untouch_nii(nii,savefilename);
 end
