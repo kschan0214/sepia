68c53bc3907e9d1fa0ac5a8379156356b94220c8
Kwok-Shing Chan <39571502+kschan0214@users.noreply.github.com>
2019-11-09 13:05:57 +0100

    update for SEGUE

--- SpecifyToolboxesDirectory.m
+++ SpecifyToolboxesDirectory.m
@@ -9,10 +9,17 @@
 % Kwok-shing Chan @ DCCN
 % k.chan@donders.ru.nl
 % Date created: 24 August 2018
-% Date last modified:
+% Date last modified: 9 November 2019
 %
 %
 %% Specify the directories of the toolbox here
 MEDI_dir = '/path/to/MEDI/Toolboxes/';
 STISuite_dir = '/path/to/STI/Suite/';
 FANSI_dir = '/path/to/FANSI/Toolboxes/';
+SEGUE_dir = '/path/to/SEGUE/Toolboxes/';
+
+%% if you have any missing toolbox, then specify it as empty array
+% MEDI_dir      = [];
+% STISuite_dir  = [];
+% FANSI_dir     = [];
+% SEGUE_dir     = [];
