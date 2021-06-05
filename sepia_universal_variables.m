54a3e64bc3adf4ae26cca146206e65089b72040d
Kwok-Shing Chan <kwocha@mentat004.dccn.nl>
2021-06-05 15:42:04 +0200

    development update

--- sepia_universal_variables.m
+++ sepia_universal_variables.m
@@ -12,7 +12,7 @@
 % DO NOT change the order of the entities, add a new one at the end instead
 %
 %% Version
-SEPIA_version = 'v0.8.1.1';
+SEPIA_version = 'v0.8.1.1dev';
 
 %% PATH
 SEPIA_HOME = fileparts(mfilename('fullpath'));
