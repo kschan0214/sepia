8934690281920a1bd0153ec4a0e309c144b31c68
Kwok-Shing Chan <reyesckshing@gmail.com>
2022-02-25 00:49:46 +0100

    final adjustment

--- SpecifyToolboxesDirectory.m
+++ SpecifyToolboxesDirectory.m
@@ -11,7 +11,7 @@
 % Date created: 24 August 2018
 % Date modified: 9 November 2019
 % Date modified: 18 July 2020 (v0.8.0)
-% Date modified: 3 August 2020 (v1.0)
+% Date modified: 25 Feb 2022 (v1.0)
 %
 %% Specify the directories of the toolbox here
 MEDI_HOME       = '/path/to/MEDI/Toolboxes/';
@@ -19,6 +19,7 @@ STISuite_HOME   = '/path/to/STI/Suite/';
 FANSI_HOME      = '/path/to/FANSI/Toolboxes/';
 SEGUE_HOME      = '/path/to/SEGUE/Toolboxes/';
 ROMEO_HOME      = '/path/to/ROMEO/Toolboxes/';
+MRISC_HOME      = '/path/to/MRISusceptibilityCalculationmethods/Toolboxes/';
 
 %% if you have any missing toolbox, then specify it as empty array
 % MEDI_HOME      = [];
@@ -26,3 +27,4 @@ ROMEO_HOME      = '/path/to/ROMEO/Toolboxes/';
 % FANSI_HOME     = [];
 % SEGUE_HOME     = [];
 % ROMEO_HOME     = [];
+% MRISC_HOME     = [];
--- sepia_addpath.m
+++ sepia_addpath.m
@@ -9,7 +9,7 @@
 % Date modified: 24 August 2018
 % Date modified: 5 June 2019
 % Date modified: 9 June 2020 (v0.8.0)
-% Date modified: 22 June 2021 (v1.0)
+% Date modified: 22 Feb 2022 (v1.0)
 %
 function sepia_addpath(method, isStartCheck)
 
@@ -100,32 +100,38 @@ end
 function CheckPathValidity(MEDI_HOME,STISuite_HOME,FANSI_HOME,SEGUE_HOME,ROMEO_HOME,MRISC_HOME)
 
 if exist(MEDI_HOME,'dir')~=7
-    warning('Please specify a correct path for MEDI toolbox in SpecifyToolboxesDirectory.m');
+%     warning('Please specify a correct path for MEDI toolbox in SpecifyToolboxesDirectory.m');
+    disp('The directory to MEDI toolbox does not exist.')
     warning('All functions related to MEDI toolbox cannot be used.');
 end
 
 if exist(STISuite_HOME,'dir')~=7
-    warning('Please specify a correct path for STI Suite in SpecifyToolboxesDirectory.m');
+%     warning('Please specify a correct path for STI Suite in SpecifyToolboxesDirectory.m');
+    disp('The directory to STI Suite does not exist.')
     warning('All functions related to STI Suite cannot be used.');
 end
 
 if exist(FANSI_HOME,'dir')~=7
-    warning('Please specify a correct path for FANSI toolbox in SpecifyToolboxesDirectory.m');
+%     warning('Please specify a correct path for FANSI toolbox in SpecifyToolboxesDirectory.m');
+    disp('The directory to FANSI toolbox does not exist.')
     warning('All functions related to FANSI toolbox cannot be used.');
 end
 
 if exist(SEGUE_HOME,'dir')~=7
-    warning('Please specify a correct path for SEGUE in SpecifyToolboxesDirectory.m');
+%     warning('Please specify a correct path for SEGUE in SpecifyToolboxesDirectory.m');
+    disp('The directory to SEGUE toolbox does not exist.')
     warning('All functions related to SEGUE cannot be used.');
 end
    
 if exist(ROMEO_HOME,'dir')~=7
-    warning('Please specify a correct path for ROMEO in SpecifyToolboxesDirectory.m');
+%     warning('Please specify a correct path for ROMEO in SpecifyToolboxesDirectory.m');
+    disp('The directory to ROMEO toolbox does not exist.')
     warning('All functions related to ROMEO cannot be used.');
 end
 
 if exist(MRISC_HOME,'dir')~=7
-    warning('Please specify a correct path for MRI Susceptibility Calculation in SpecifyToolboxesDirectory.m');
+%     warning('Please specify a correct path for MRI Susceptibility Calculation in SpecifyToolboxesDirectory.m');
+    disp('The directory to MRI Susceptibility Calculation Methods does not exist.')
     warning('All functions related to MRI Susceptibility Calculation cannot be used.');
 end
     
