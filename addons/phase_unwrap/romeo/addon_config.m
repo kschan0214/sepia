6f730c0758b0f12a664d3a199c7e50af18788163
Korbinian Eckstein <korbinian90@gmail.com>
2022-09-27 11:33:22 +1000

    method name should stay ROMEO

--- addons/phase_unwrap/romeo/addon_config.m
+++ addons/phase_unwrap/romeo/addon_config.m
@@ -12,7 +12,7 @@
 %% main
 
 % This name will be used thorough the SEPIA framework for usage
-addons.method = 'MRITOOLS';
+addons.method = 'ROMEO';
 
 % Specify the filename of the wrapper function (without extension)
 addons.wrapper_function	= 'Wrapper_Unwrap_ROMEO_singleEcho';
