858569c96c7dbb129acdad686c988da75dda0e70
Kwok-Shing Chan <kwocha@mentat003.dccn.nl>
2023-04-03 15:46:01 +0200

    fix bug when loading pipeline configuration file to the GUI for VSHARP & FANSI

--- gui_func/callback/config/get_set_bfr_vsharp.m
+++ gui_func/callback/config/get_set_bfr_vsharp.m
@@ -37,12 +37,32 @@ switch lower(mode)
         % max radius
         pattern_curr    = str_pattern{1};
         val             = get_num_as_string(config_txt, pattern_curr, '[', ':');
+        if isempty(val)
+            % in case of machine generated config file
+            val             = get_num_as_string(config_txt, pattern_curr, '[', '\s');
+        end
         set_non_nan_value(action_handle{1},'String',val)
 
         % min radius
-        val             = get_num_as_string(config_txt, pattern_curr, ':', ']');
-        idx             = regexp(val,':');
-        val             = val(idx+1:end);
+        % 20230329: bug fix for sepiaIO auto-generated pipeline configuration file
+        try
+            val             = get_num_as_string(config_txt, pattern_curr, ':', ']');
+            idx             = regexp(val,':');
+            val             = val(idx+1:end);
+        catch
+            idx_open        = regexp(config_txt,'[');
+            idx_close       = regexp(config_txt,']');
+            idx_pattern     = regexp(config_txt,pattern_curr);
+            
+            idx_first       = find(idx_open>idx_pattern);
+            idx_last        = find(idx_close>idx_pattern);
+            
+            val_text        = config_txt(idx_open(idx_first):idx_close(idx_last));
+            val_num         = str2num(val_text);
+            
+            val             = min(val_num);
+        end
+        
         set_non_nan_value(action_handle{2},'String',val)
 
 end
